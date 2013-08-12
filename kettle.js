87b4c8368a826288dcb911cfaefd2316c765db3f
Yura Zenevich <yura.zenevich@gmail.com>
2013-08-12 12:25:35 -0400

    GPII-116: updated copyright and added comments.

--- kettle.js
+++ kettle.js
@@ -1,3 +1,15 @@
+/*
+Kettle.
+
+Copyright 2012-2013 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/kettle/LICENSE.txt
+*/
+
 var fluid = require("infusion"),
     kettle = fluid.registerNamespace("kettle");
 
