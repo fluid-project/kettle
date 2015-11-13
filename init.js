9c70fd534bb4ad7926119faeefb3e2b2ec156ca1
Antranig Basman <antranig.basman@colorado.edu>
2015-11-13 11:45:54 +0000

    KETTLE-32: Readded init.js and added some basically courteous diagnostics for the loading process

---
+++ init.js
@@ -0,0 +1,19 @@
+/*
+Kettle Init - Bootstrap for Kettle "configs"
+
+Copyright 2012-2013 OCAD University
+Copyright 2015 Raising the Floor - International
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+You may obtain a copy of the License at
+https://github.com/gpii/kettle/LICENSE.txt
+*/
+
+"use strict";
+
+var kettle = require("./kettle.js");
+
+kettle.config.makeConfigLoader({
+    configName: kettle.config.getNodeEnv(),
+    configPath: kettle.config.getConfigPath()
+});
