2517cc1d9d047d58aef70db48d1580da718ffc16
Antranig Basman <antranig.basman@colorado.edu>
2015-12-19 02:40:09 +0000

    KETTLE-32: Comment update

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,6 @@ You may obtain a copy of the ECL 2.0 License and BSD License at
 https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 */
 
-// Declare dependencies
-
 module.exports = function(grunt) {
     "use strict";
 
