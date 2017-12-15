545d427005b380237002f59c7d21b391627eb810
Alan Harnum <aharnum@ocadu.ca>
2017-12-15 14:14:32 -0500

    KETTLE-66: test for trying to upload files beyond an array maxlimit.

--- tests/MulterTests.js
+++ tests/MulterTests.js
@@ -60,7 +60,7 @@ kettle.tests.multerHandlerArray = function (request) {
 
 kettle.tests["multer"].testDefs = [{
     name: "Multer tests",
-    expect: 2,
+    expect: 6,
     config: {
         configName: "kettle.tests.multer.config",
         configPath: "%kettle/tests/configs"
@@ -85,7 +85,19 @@ kettle.tests["multer"].testDefs = [{
                 method: "POST",
                 formData: {
                     files: {
-                        "files": ["./LICENSE.txt", "./README.md"]
+                        "files": ["./README.md", "./LICENSE.txt"]
+                    }
+                }
+            }
+        },
+        arrayFileUploadTooMany: {
+            type: "kettle.test.request.formData",
+            options: {
+                path: "/multer-array",
+                method: "POST",
+                formData: {
+                    files: {
+                        "files": ["./README.md", "./LICENSE.txt", "./History.md"]
                     }
                 }
             }
@@ -101,6 +113,11 @@ kettle.tests["multer"].testDefs = [{
     },{
         event: "{arrayFileUpload}.events.onComplete",
         listener: "kettle.test.testMulterArray"
+    },{
+        func: "{arrayFileUploadTooMany}.send"
+    },{
+        event: "{arrayFileUploadTooMany}.events.onComplete",
+        listener: "kettle.test.testMulterArrayTooMany"
     }]
 }];
 
@@ -109,10 +126,19 @@ kettle.test.testMulterSingle = function (fileInfo, that) {
     jqUnit.assertEquals("file name is expected", "LICENSE.txt", parsedFileInfo.originalname);
 };
 
-kettle.test.testMulterArray = function (fileInfo, that) {
-    var parsedFileInfo = JSON.parse(fileInfo);
-    console.log(parsedFileInfo);
-    jqUnit.assertEquals("files length is expected", 2, parsedFileInfo.length);
+kettle.test.testMulterArray = function (filesInfo, that) {
+    var parsedFilesInfo = JSON.parse(filesInfo);
+    var expectedFileNames = ["README.md", "LICENSE.txt"]; jqUnit.assertEquals("files length is expected", 2, parsedFilesInfo.length);
+    fluid.each(parsedFilesInfo, function (fileInfo, idx) {
+        jqUnit.assertEquals("file name is expected", expectedFileNames[idx], fileInfo.originalname);
+    });
+
+};
+
+kettle.test.testMulterArrayTooMany = function (filesInfo) {
+    var parsedFilesInfo = JSON.parse(filesInfo);
+    jqUnit.assertTrue("Trying to upload more files than the maxcount throws an error", parsedFilesInfo.isError);
+    jqUnit.assertEquals("Error code is expected multer LIMIT_UNEXPECTED_FILE code", "LIMIT_UNEXPECTED_FILE", parsedFilesInfo.code)
 };
 
 kettle.test.bootstrapServer(kettle.tests["multer"].testDefs);
