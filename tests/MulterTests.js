3dca817b6dee1a92e524f7a14dba1afc2d8dc2e9
Alan Harnum <aharnum@ocadu.ca>
2017-12-15 12:59:39 -0500

    KETTLE-66: some basic working tests + implementation.

--- tests/MulterTests.js
+++ tests/MulterTests.js
@@ -20,29 +20,47 @@ kettle.loadTestingSupport();
 
 fluid.registerNamespace("kettle.tests.multer");
 
-fluid.defaults("kettle.tests.multer.handler", {
+fluid.defaults("kettle.tests.multer.handler.single", {
     gradeNames: "kettle.request.http",
     requestMiddleware: {
-        "multer": {
-            middleware: "{server}.infusionMulter"
+        "multerSingle": {
+            middleware: "{server}.infusionMulterSingle"
         }
     },
     invokers: {
         handleRequest: {
-            funcName: "kettle.tests.multerHandler"
+            funcName: "kettle.tests.multerHandlerSingle"
         }
     }
 });
 
-kettle.tests.multerHandler = function (request) {
+// Sends the info about the uploaded file
+kettle.tests.multerHandlerSingle = function (request) {
     request.events.onSuccess.fire(request.req.file);
 };
 
+fluid.defaults("kettle.tests.multer.handler.array", {
+    gradeNames: "kettle.request.http",
+    requestMiddleware: {
+        "multerArray": {
+            middleware: "{server}.infusionMulterArray"
+        }
+    },
+    invokers: {
+        handleRequest: {
+            funcName: "kettle.tests.multerHandlerArray"
+        }
+    }
+});
 
-//------------- Test defs for GET, POST, PUT ---------------
+// Sends the info about the uploaded files
+kettle.tests.multerHandlerArray = function (request) {
+    request.events.onSuccess.fire(request.req.files);
+};
+
 kettle.tests["multer"].testDefs = [{
-    name: "Multer POST test",
-    expect: 1,
+    name: "Multer tests",
+    expect: 2,
     config: {
         configName: "kettle.tests.multer.config",
         configPath: "%kettle/tests/configs"
@@ -51,11 +69,23 @@ kettle.tests["multer"].testDefs = [{
         singleFileUpload: {
             type: "kettle.test.request.formData",
             options: {
-                path: "/multer/1",
+                path: "/multer-single",
                 method: "POST",
                 formData: {
                     files: {
                         "file": "./LICENSE.txt"
+                    }
+                }
+            }
+        },
+        arrayFileUpload: {
+            type: "kettle.test.request.formData",
+            options: {
+                path: "/multer-array",
+                method: "POST",
+                formData: {
+                    files: {
+                        "files": ["./LICENSE.txt", "./README.md"]
                     }
                 }
             }
@@ -66,12 +96,23 @@ kettle.tests["multer"].testDefs = [{
     }, {
         event: "{singleFileUpload}.events.onComplete",
         listener: "kettle.test.testMulterSingle"
+    },{
+        func: "{arrayFileUpload}.send"
+    },{
+        event: "{arrayFileUpload}.events.onComplete",
+        listener: "kettle.test.testMulterArray"
     }]
 }];
 
 kettle.test.testMulterSingle = function (fileInfo, that) {
     var parsedFileInfo = JSON.parse(fileInfo);
     jqUnit.assertEquals("file name is expected", "LICENSE.txt", parsedFileInfo.originalname);
+};
+
+kettle.test.testMulterArray = function (fileInfo, that) {
+    var parsedFileInfo = JSON.parse(fileInfo);
+    console.log(parsedFileInfo);
+    jqUnit.assertEquals("files length is expected", 2, parsedFileInfo.length);
 };
 
 kettle.test.bootstrapServer(kettle.tests["multer"].testDefs);
