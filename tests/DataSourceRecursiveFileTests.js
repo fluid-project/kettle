8c79f96f19de1c09126762a74e2b484b1c40b71e
Kasper Markus <kasper@markus.dk>
2018-01-04 11:28:00 +0100

    KETTLE-63: Now does a scan at startup instead of for each request

--- tests/DataSourceRecursiveFileTests.js
+++ tests/DataSourceRecursiveFileTests.js
@@ -35,7 +35,6 @@ fluid.defaults("kettle.tests.dataSource.1.recursiveFile", {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data/",
                 filename: "dataSourceTestFile.json"
             }
         }
@@ -57,7 +56,6 @@ fluid.defaults("kettle.tests.dataSource.2.recursiveFile", {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data",
                 filename: "dataSourceTestFile.json"
             }
         }
@@ -79,7 +77,6 @@ fluid.defaults("kettle.tests.dataSource.3.recursiveFile", {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data/",
                 filename: "nestedFile.json"
             }
         }
@@ -101,7 +98,6 @@ fluid.defaults("kettle.tests.dataSource.4.recursiveFile", {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data/",
                 filename: "doubleNestedFile.json"
             }
         }
@@ -124,7 +120,6 @@ fluid.defaults("kettle.tests.dataSource.5.recursiveFile", {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data/",
                 filename: "Idontexist.json"
             }
         }
@@ -134,7 +129,7 @@ fluid.defaults("kettle.tests.dataSource.5.recursiveFile", {
             funcName: "kettle.tests.dataSource.testErrorResponse",
             args: [{
                 isError: true,
-                message: fluid.module.resolvePath("File Idontexist.json was not found when searching from path %kettle/tests/data/"),
+                message: fluid.module.resolvePath("File Idontexist.json was not found when searching from path " + process.cwd()),
                 statusCode: 404
             }, "{arguments}.0"]
         }
@@ -143,63 +138,12 @@ fluid.defaults("kettle.tests.dataSource.5.recursiveFile", {
 
 fluid.defaults("kettle.tests.dataSource.6.recursiveFile", {
     gradeNames: ["kettle.tests.simpleDataSourceTest"],
-    name: "Recursive file datasource: path is not a folder",
-    shouldError: true,
-    components: {
-        dataSource: {
-            type: "kettle.dataSource.recursiveFile.moduleTerms",
-            options: {
-                path: "%kettle/tests/data/dataSourceTestFile.json",
-                filename: "dataSourceTestFile.json"
-            }
-        }
-    },
-    invokers: {
-        errorFunc: {
-            funcName: "kettle.tests.dataSource.testErrorResponse",
-            args: [{
-                isError: true,
-                message: fluid.module.resolvePath("Error while searching for file... ENOTDIR: not a directory, scandir '%kettle/tests/data/dataSourceTestFile.json'"),
-                statusCode: 500
-            }, "{arguments}.0"]
-        }
-    }
-});
-
-fluid.defaults("kettle.tests.dataSource.7.recursiveFile", {
-    gradeNames: ["kettle.tests.simpleDataSourceTest"],
-    name: "Recursive file datasource: path is not existent",
-    shouldError: true,
-    components: {
-        dataSource: {
-            type: "kettle.dataSource.recursiveFile.moduleTerms",
-            options: {
-                path: "%kettle/tests/idontexistdir",
-                filename: "dataSourceTestFile.json"
-            }
-        }
-    },
-    invokers: {
-        errorFunc: {
-            funcName: "kettle.tests.dataSource.testErrorResponse",
-            args: [{
-                isError: true,
-                message: fluid.module.resolvePath("Error while searching for file... ENOENT: no such file or directory, scandir '%kettle/tests/idontexistdir'"),
-                statusCode: 500
-            }, "{arguments}.0"]
-        }
-    }
-});
-
-fluid.defaults("kettle.tests.dataSource.8.recursiveFile", {
-    gradeNames: ["kettle.tests.simpleDataSourceTest"],
     name: "Recursive file datasource: folder has same name as file - it wont count as a hit",
     shouldError: true,
     components: {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data",
                 filename: "nested"
             }
         }
@@ -209,21 +153,20 @@ fluid.defaults("kettle.tests.dataSource.8.recursiveFile", {
             funcName: "kettle.tests.dataSource.testErrorResponse",
             args: [{
                 isError: true,
-                message: fluid.module.resolvePath("File nested was not found when searching from path %kettle/tests/data"),
+                message: fluid.module.resolvePath("File nested was not found when searching from path " + process.cwd()),
                 statusCode: 404
             }, "{arguments}.0"]
         }
     }
 });
 
-fluid.defaults("kettle.tests.dataSource.9.recursiveFile", {
+fluid.defaults("kettle.tests.dataSource.7.recursiveFile", {
     gradeNames: ["kettle.tests.simpleDataSourceTest"],
     name: "Recursive file datasource: multiple hits of the same file... Same file is always read",
     components: {
         dataSource: {
             type: "kettle.dataSource.recursiveFile.moduleTerms",
             options: {
-                path: "%kettle/tests/data",
                 filename: "commonFileName.json"
             }
         }
@@ -245,9 +188,7 @@ kettle.tests.dataSource.standardTests = [
     "kettle.tests.dataSource.4.recursiveFile",
     "kettle.tests.dataSource.5.recursiveFile",
     "kettle.tests.dataSource.6.recursiveFile",
-    "kettle.tests.dataSource.7.recursiveFile",
-    "kettle.tests.dataSource.8.recursiveFile",
-    "kettle.tests.dataSource.9.recursiveFile"
+    "kettle.tests.dataSource.7.recursiveFile"
 ];
 
 // Convert each of the standard test fixture grades into tests of the equivalent promise-based API
